630adf9ca1f70e9dbb90a560e9583958089dc7c5
sulekh <73203721+sulekh-prog@users.noreply.github.com>
2021-07-18 23:36:29 +1200

    connected with mongodb

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+const URL =
+  "mongodb+srv://sulekh:sulekh123@cluster0.q3yx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
-);
+app.get("/", (req, res) => res.send(`Store server running on port ${PORT}`));
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
